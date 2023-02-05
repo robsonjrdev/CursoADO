c53381416f130c34987254141761ddf1a1fde5be
Robson Junior <96668628+robsonjrdev@users.noreply.github.com>
2023-02-05 19:13:30 -0300

    Add project files.

---
+++ CursoADO/Conexao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoADO
+{
+    public class Conexao
+    {
+        private static string connString = "Server=localhost;Database=ADWI; User Id=robson;Password=robson";
+
+        public static SqlConnection ConnSQL()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                return conn;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Erro ao se conectar ao banco");
+            }
+          
+        }
+    }
+}
---
+++ CursoADO/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CursoADO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //SalvarCliente("Islana", "islanasilvaa@gmail.com", "123456");
+            ExcluirCliente(3);
+            ListarClientes();
+        }
+        public static void ListarClientes()
+        {
+            using (var conn = Conexao.ConnSQL())
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM Cliente order by id_cliente";
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine(dr["Nome"]);
+                        Console.WriteLine(dr["Email"]);
+                    }
+                }
+            }
+        }
+        public static void SalvarCliente(string nome, string email, string senha)
+        {
+            using (var conn = Conexao.ConnSQL())
+            {
+                try
+                {
+                    
+                    SqlCommand cmd = conn.CreateCommand();
+                    conn.Open();
+                    cmd.CommandText = "INSERT INTO Cliente (Nome, Email, Senha) VALUES(@Nome, @Email, @Senha)";
+                    cmd.Parameters.AddWithValue("@Nome", nome);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@Senha", senha);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+
+                    throw new Exception("Erro ao salvar cliente.");
+                }
+
+            }
+        }
+        public static void ExcluirCliente(int id)
+        {
+            using (var conn = Conexao.ConnSQL())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE Cliente WHERE Id_Cliente = @id", conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+
+                    throw new Exception($"Erro ao excluir cliente. {e.Message}");
+                }
+
+
+            }
+        }
+    }
+} 
